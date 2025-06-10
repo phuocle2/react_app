1d28da3a0f8f295ece87b93e0f8d0eeebaa17a39
phuocle2 <issah.le@gmail.com>
2025-06-10 03:04:19 -0500

    testing...

--- components/MainPage.js
+++ components/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext, useCallback, request } from 'react';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useLocation,
 } from 'react-router-dom';
 
 import MainPage from './components/MainPage';
