6d27b13d441da6b391d35e2caf1aa7fe8595cb66
phuocle2 <issah.le@gmail.com>
2025-06-10 03:27:41 -0500

    testing...

--- src/components/store/auth.js
+++ src/components/store/auth.js
@@ -3,7 +3,9 @@ import React from 'react';
 const AuthContext = React.createContext({
     version: '0.1.0',
     greeting: 'http://192.168.1.186:5001/home',
-    profile: 'http://192.168.1.186:5001/profile',
-    update: 'http://192.168.1.186:5001/update',
+    // profile: 'http://192.168.1.186:5001/profile',
+    // update: 'http://192.168.1.186:5001/update',
+    profile: 'https://cheaply-firm-quagga.ngrok-free.app/profile',
+    update: 'https://cheaply-firm-quagga.ngrok-free.app/update',
 })
 export default AuthContext;
